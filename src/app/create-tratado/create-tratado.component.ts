3aeb5ee1d888c13058151142ac4e0f8277ffaa2b
AdolfAlcantara <raulalcantara_96@hotmail.com>
2019-12-15 23:36:45 -0600

    modified create-tratado.component.ts class to handle implement on calling ethereum contract

--- src/app/create-tratado/create-tratado.component.ts
+++ src/app/create-tratado/create-tratado.component.ts
@@ -2,7 +2,10 @@ import {TratadoService} from '../tratado.service';
 import {Tratado} from '../tratado';
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import Web3 from 'web3'; 
 
+var web3: any;
+var contract: any;
 
 @Component({
   selector: 'app-create-tratado',
@@ -15,7 +18,64 @@ export class CreateTratadoComponent implements OnInit {
   submitted = false;
 
   constructor(private tratadoService: TratadoService,
-    private router: Router) { }
+    private router: Router) { 
+
+
+    if(typeof web3 !== 'undefined'){
+      web3 = new Web3(web3.currentProvider);
+    }else{
+      web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    }
+
+    web3.eth.defaultAccount = web3.eth.accounts[0];
+    contract = web3.eth.contract([
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "owner",
+            "type": "string"
+          },
+          {
+            "name": "property",
+            "type": "string"
+          },
+          {
+            "name": "time",
+            "type": "string"
+          }
+        ],
+        "name": "setInfo",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "getInfo",
+        "outputs": [
+          {
+            "name": "",
+            "type": "string"
+          },
+          {
+            "name": "",
+            "type": "string"
+          },
+          {
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      }
+    ]);
+
+  }
 
   ngOnInit() {
   }
@@ -35,12 +95,18 @@ export class CreateTratadoComponent implements OnInit {
   }
 
   onSubmit(){
+    this.createSmartContract();
     this.submitted = true;
     this.save();
   }
 
   gotoList(){
     this.router.navigate(['/tratados']);
+  }
+
+  createSmartContract(){
+    let myContract = contract.at("0xf8585fE2Bc44C86d5eD744b0A85A2725238E35dE");
+    myContract.setInfo(this.tratado.propietario, this.tratado.propiedad,this.tratado.fecha);
   }
 
 }
